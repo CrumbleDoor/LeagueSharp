2b102ed70575ffaaf55dad29d5423b9ee4bf78e2
Baballev <baballevincent@gmail.com>
2015-10-25 01:46:29 +0200

    frist commit Leplank

---
+++ Leplank/Leplank/Program.cs
@@ -0,0 +1,22 @@
+﻿using LeagueSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+
+namespace Leplank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
