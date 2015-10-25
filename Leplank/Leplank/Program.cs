b932076f4186e36165b975b14f0effe1be024c8c
Brikovich <nochex37@gmail.com>
2015-10-25 11:52:37 +0100

    upd

--- Leplank/Leplank/Program.cs
+++ Leplank/Leplank/Program.cs
@@ -14,10 +14,25 @@ namespace Leplank
 {
     class Program
     {
+        #region declarations
+        static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+        private static Spell Q, W, E, R;
+        static string champName = "Gangplank";
+        public const string version = "1.0.0.0";
+        #endregion declarations
+
         static void Main(string[] args)
         {
-            Game.PrintChat("ohayo - test");
-            Game.PrintChat("Kreygasm ça marche");
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Game.PrintChat("<b><font color='#FF6600'>Leplank</font></b> " + version + " loaded - By <font color='#6666FF'>Brikovich</font> & <font color='#6666FF'>Baballev</font>");
+            Game.PrintChat("Don't forget to <font color='#00CC00'><b>Upvote</b></font> <b><font color='#FF6600'>Leplank</font></b> in the Assembly DB if you like it ^_^");
+            Menus.MenuIni();
+        }
+
+
     }
 }
